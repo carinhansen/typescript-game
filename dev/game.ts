bce50c01e80a315f7bee018cc6cb07ac3db2d6a6
Carin Hansen <carinhansen95@gmail.com>
2018-05-24 15:56:34 +0200

    Character with movement added

--- dev/game.ts
+++ dev/game.ts
@@ -1,7 +1,26 @@
 class Game {
-    constructor(){
-        console.log("new game created!")
+    private static instance: Game;
+    private c: Character;
+
+    private constructor() {
+        console.log("New Game")
+        this.c = new Character();
+        this.gameLoop()
+    }
+
+    public static getInstance() {
+        if (! Game.instance) {
+            Game.instance = new Game();
+        }
+        return Game.instance;
+    }
+
+    gameLoop() {
+        this.c.update();
+        requestAnimationFrame(() => this.gameLoop())
     }
 }
 
-window.addEventListener("load", () => new Game())
+   window.addEventListener("load", ()=> { Game.getInstance() })
+
+
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,67 @@
 "use strict";
+var Character = (function () {
+    function Character() {
+        var _this = this;
+        this.leftSpeed = 0;
+        this.rightSpeed = 0;
+        this.downSpeed = 0;
+        this.upSpeed = 0;
+        this.htmlElement = document.createElement("div");
+        document.body.appendChild(this.htmlElement).className = "character";
+        this.posx = window.innerWidth / 2 - 125;
+        this.posy = window.innerHeight - 250;
+        this.htmlElement.style.transform = "translate(" + this.posx + "px, " + this.posy + "px)";
+        window.addEventListener("keydown", function (e) { return _this.onKeyDown(e); });
+        window.addEventListener("keyup", function (e) { return _this.onKeyUp(e); });
+    }
+    Character.prototype.update = function () {
+        if (this.leftSpeed) {
+            this.htmlElement.style.transform = "translate(" + (this.posx -= this.leftSpeed) + "px, " + this.posy + "px)";
+        }
+        if (this.rightSpeed) {
+            this.htmlElement.style.transform = "translate(" + (this.posx += this.rightSpeed) + "px, " + this.posy + "px)";
+        }
+    };
+    Character.prototype.onKeyDown = function (event) {
+        switch (event.keyCode) {
+            case 65:
+                this.leftSpeed = 5;
+                break;
+            case 68:
+                this.rightSpeed = 5;
+                break;
+        }
+    };
+    Character.prototype.onKeyUp = function (event) {
+        switch (event.keyCode) {
+            case 65:
+                this.leftSpeed = 0;
+                break;
+            case 68:
+                this.rightSpeed = 0;
+                break;
+        }
+    };
+    return Character;
+}());
 var Game = (function () {
     function Game() {
-        console.log("new game created!");
+        console.log("New Game");
+        this.c = new Character();
+        this.gameLoop();
     }
+    Game.getInstance = function () {
+        if (!Game.instance) {
+            Game.instance = new Game();
+        }
+        return Game.instance;
+    };
+    Game.prototype.gameLoop = function () {
+        var _this = this;
+        this.c.update();
+        requestAnimationFrame(function () { return _this.gameLoop(); });
+    };
     return Game;
 }());
-window.addEventListener("load", function () { return new Game(); });
+window.addEventListener("load", function () { Game.getInstance(); });
 //# sourceMappingURL=main.js.map
